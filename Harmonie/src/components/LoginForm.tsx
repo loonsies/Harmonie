b5452a71d1d0326ee6a04a74df6119d0881f0436
senolem <37041587+senolem@users.noreply.github.com>
2025-02-07 09:50:22 +0100

    added register button

--- Harmonie/src/components/LoginForm.tsx
+++ Harmonie/src/components/LoginForm.tsx
@@ -92,6 +92,12 @@ export default function LoginForm() {
           >
             Sign in
           </button>
+          <button
+            type="submit"
+            className="flex w-full justify-center rounded-md bg-white px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-gray-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray-600"
+          >
+            Register
+          </button>
         </div>
       </form>
     </div>
