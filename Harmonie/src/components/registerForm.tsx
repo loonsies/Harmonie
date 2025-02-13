7132c0f53543bae00e2ebe8f5458f599143bc0c7
loonsies <me@looney.boo>
2025-02-13 12:56:19 +0100

    fix tos and privacy policy appearance in register

--- Harmonie/src/components/registerForm.tsx
+++ Harmonie/src/components/registerForm.tsx
@@ -110,8 +110,8 @@ export function RegisterForm({
           </form>
         </CardContent>
       </Card>
-      <div className="text-center text-xs text-muted-foreground">
-        By clicking continue, you agree to our <a href="#">Terms of Service</a>
+      <div className="text-center text-xs text-muted-foreground [&_a]:underline [&_a]:underline-offset-4 [&_a]:hover:text-primary">
+        By clicking continue, you agree to our <a href="#">Terms of Service</a>{" "}
         and <a href="#">Privacy Policy</a>.
       </div>
     </div>
